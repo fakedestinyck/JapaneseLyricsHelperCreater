dc0ff27a92cb9834764be625dc88bb19dec953f3
Haochang Qian <fakedestinyck@gmail.com>
2017-12-17 10:46:40 -0500

    Initial Commit

---
+++ JapaneseLyricsHelperCreater/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  JapaneseLyricsHelperCreater
+//
+//  Created by  on Dec/17/2017.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
